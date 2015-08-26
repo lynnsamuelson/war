173f18da6176a7a8381355c3e71117e87b34ceab
Lynn Samuelson <lynn.samuelson@gmail.com>
2015-08-26 09:01:39 -0500

    added styling

--- javascripts/main.js
+++ javascripts/main.js
@@ -20,40 +20,7 @@ requirejs(["dependencies", "firebase", "get-deck", "draw-card", "draw"],
   function(dependencies, firebase, getDeck, drawCard, draw) {
 
     draw();
-    //gamePlay();
-    // var deckOne = getDeck();
-    // var cardOneValue;
-    // var cardTwoValue;
-    // deckOne.then(function(deck) {
-    //   $('#deckOne').on("click", function() {
-    //     drawCard(deck).then(function(card) {
-    //     //console.log("Deck One", card);
-    //     var cardImage = "<img src=" + card.cards[0].image + "></img>";
-    //     $('#cardOne').html(cardImage);
-    //     console.log("card one", card.cards[0].value);
-    //     cardOneValue = card.cards[0].value;
-        
-    //     });
-    //   });
-    // });
-
-    // var deckTwo = getDeck();
-    // deckTwo.then(function(deck) {
-    //   $('#deckTwo').on("click", function() {
-    //     drawCard(deck).then(function(card) {
-    //     //console.log("Deck two", card);
-    //     var cardImage = "<img src=" + card.cards[0].image + "></img>";
-    //     $('#cardTwo').html(cardImage);
-    //     console.log("card two", card.cards[0].value);
-    //     cardTwoValue = card.cards[0].value;
-    //     });
-    //   });
-    // });
-
+    
 
   });
 
-
-//create two decks 
-//event handler for drawing cards
-//logic for drawing a card on each deck
